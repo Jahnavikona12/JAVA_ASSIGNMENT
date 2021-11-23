c19346702f2a74be82ab7b9443c623ab9b098f0c
Jahnavikona12 <94894041+Jahnavikona12@users.noreply.github.com>
2021-11-23 13:42:11 +0530

    Create Arithmeticexception.java

---
+++ Arithmeticexception.java
@@ -0,0 +1,12 @@
+package com.broadridge.assignment;
+
+public class Arithmeticexception {
+
+	public static void main(String[] args) throws ArithmeticException{
+
+		int x = 10, y = 0;
+		int z = x / y;
+		} 
+
+}
+	
